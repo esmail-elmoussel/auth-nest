91912f28d2188defc679a544a7a173adbb138945
Esmail Elmoussel <elmoussel12@gmail.com>
2023-11-18 21:22:17 +0200

    feat(login): handle login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Controller('/auth')
 export class AuthController {
@@ -12,7 +13,16 @@ export class AuthController {
   async register(@Body() body: CreateUserDto) {
     const registeredUser = await this.authService.register(body);
 
-    this.logger.log(`user ${registeredUser.id} registered successfully!`);
+    this.logger.log(`User ${registeredUser.id} registered successfully!`);
+
+    return registeredUser;
+  }
+
+  @Post('/login')
+  async login(@Body() body: LoginUserDto) {
+    const registeredUser = await this.authService.login(body);
+
+    this.logger.log(`User ${registeredUser.id} registered successfully!`);
 
     return registeredUser;
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { AuthRepository } from './auth.repository';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { HashUtility } from 'src/auth/utils/hash.utility';
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,9 +17,9 @@ export class AuthService {
     const user = await this.authRepository.findOneByEmail(createUserDto.email);
 
     if (user) {
-      this.logger.log(`user ${user.id} already exists!`);
+      this.logger.log(`User ${user.id} already exists!`);
 
-      throw new BadRequestException('Account already exists!');
+      throw new BadRequestException('Invalid credentials!'); // for security purposes, not to expose the existence of a user!
     }
 
     const hashedPassword = await this.hashUtility.hash(createUserDto.password);
@@ -29,8 +30,33 @@ export class AuthService {
       password: hashedPassword,
     });
 
-    this.logger.log(`user ${newUser.id} created successfully!`);
+    this.logger.log(`User ${newUser.id} created successfully!`);
 
     return newUser;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.authRepository.findOneByEmail(loginUserDto.email);
+
+    if (!user) {
+      this.logger.log(`User ${loginUserDto.email} not exists!`);
+
+      throw new BadRequestException('Invalid credentials!'); // for security purposes, not to expose the existence of a user!
+    }
+
+    const isPasswordCorrect = await this.hashUtility.validate(
+      loginUserDto.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrect) {
+      this.logger.log(`Incorrect password for ${user.id}`);
+
+      throw new BadRequestException('Invalid credentials!'); // for security purposes, not to expose the existence of a user!
+    }
+
+    this.logger.log(`User ${user.id} logged in successfully!`);
+
+    return user;
   }
 }
