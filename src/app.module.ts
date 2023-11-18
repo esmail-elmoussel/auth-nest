46cacc94476453246d3e3cb6ebc0381fd2197242
Esmail Elmoussel <elmoussel12@gmail.com>
2023-11-18 20:32:19 +0200

    feat(database): connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig } from './types/global.types';
 import { validateEnvVariables } from './config';
 
 @Module({
@@ -10,6 +12,17 @@ import { validateEnvVariables } from './config';
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnvVariables,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<AppConfig>) => ({
+        type: 'mongodb',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
   ],
