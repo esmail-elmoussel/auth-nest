9b890058a4dfcea603935a9ec562b240bcd807d3
Esmail Elmoussel <elmoussel12@gmail.com>
2023-11-18 21:20:44 +0200

    fix(HashUtility): fix validate method

--- src/auth/utils/hash.utility.ts
+++ src/auth/utils/hash.utility.ts
@@ -4,20 +4,23 @@ import { promisify } from 'util';
 const scrypt = promisify(_scrypt);
 
 export class HashUtility {
-  async hash(password: string): Promise<string> {
+  async hash(plainTextPassword: string): Promise<string> {
     const salt = randomBytes(8).toString('hex');
 
-    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const hash = (await scrypt(plainTextPassword, salt, 32)) as Buffer;
 
     const saltWithHash = salt + '.' + hash.toString('hex');
 
     return saltWithHash;
   }
 
-  async validate(password: string): Promise<boolean> {
-    const [salt, hashedPassword] = password.split('.');
+  async validate(
+    plainTextPassword: string,
+    saltWithHash: string,
+  ): Promise<boolean> {
+    const [salt, hashedPassword] = saltWithHash.split('.');
 
-    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const hash = (await scrypt(plainTextPassword, salt, 32)) as Buffer;
 
     if (hashedPassword !== hash.toString('hex')) {
       return false;
