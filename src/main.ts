d9408b43206186dfb128850cb4ea4e6a67be2d75
Esmail Elmoussel <elmoussel12@gmail.com>
2023-11-18 20:40:33 +0200

    feat: get app port from env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppConfig } from './types/global.types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService<AppConfig>);
+
+  const port = configService.get<number>('PORT', 3000);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,7 +18,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 
 bootstrap();
